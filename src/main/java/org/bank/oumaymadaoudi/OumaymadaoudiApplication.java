d2fa50872c7b1f9a0f99cfc869d573e6d9825201
Ouma49 <192384810+Ouma49@users.noreply.github.com>
2025-05-19 09:04:25 +0100

    Initial commit

---
+++ src/main/java/org/bank/oumaymadaoudi/OumaymadaoudiApplication.java
@@ -0,0 +1,13 @@
+package org.bank.oumaymadaoudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OumaymadaoudiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OumaymadaoudiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bank/oumaymadaoudi/OumaymadaoudiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bank.oumaymadaoudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OumaymadaoudiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
